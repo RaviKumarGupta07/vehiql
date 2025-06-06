3aa390eb7da93993a7ecf0cd676425536e66b086
Ravi Kumar Gupta <ravikumarg05082004@gmail.com>
2025-06-06 12:34:31 +0530

    Added Cars Admin Page

---
+++ app/(admin)/admin/_components/cars/create/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Add New Car | Vehiql Admin",
+  description: "Add a new car to the marketplace",
+};
+
+const CarsPage = () => {
+  return <div>CarsPage</div>
+}
+
+export default CarsPage
